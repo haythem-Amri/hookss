266b185faf81f3121f88ac1ba012e3aca20bf187
Haythem amri <92266198+haythem-Amri@users.noreply.github.com>
2021-12-11 01:41:20 +0100

    Initial commit

---
+++ hooks/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import MovieList2 from './Components/MovieList2';
+
+
+
+function App() {
+  
+  
+
+
+  return (
+
+    <>
+    <MovieList2></MovieList2>
+    </>
+  );
+}
+
+export default App;
